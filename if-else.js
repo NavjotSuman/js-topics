60d133e4ad6718b399a6e099e7749112739a03af
Navjot Suman <navjotsumandev@gmail.com>
2026-02-03 12:40:13 +0530

    initial commit

---
+++ if-else.js
@@ -0,0 +1,83 @@
+// if -> (10 > 8) -> run if
+// else -> else
+
+
+
+// if -> single if
+
+// if -> signle if 1
+// if -> signle if 2
+
+// if
+// else if
+// else if
+// else if
+// .
+// .
+// else if
+// else
+
+
+// if -> smaan mil gyia
+// else -> agan smaan nai mile ta eh item le ayo!
+
+
+
+console.log("Starting...");
+
+let a = 10
+let b = 8
+// if(a < b){
+//     console.log(`${a} is Bigger than ${b}`);
+// }
+// if(a === 10){
+//     console.log("a is 10");
+// }
+// else if(a === 10){
+//     console.log("a is 20");
+//     // lines of code
+// }
+
+
+
+// if-else
+
+// if (a > b) {
+//     console.log("A is bigger than B");
+//     if(a === 10){
+//         console.log("a === 10");
+//     } else console.log("A is not Equeal to 10");
+// }
+// else{
+//     console.log("A is not bigger than B");
+// }
+// console.log("ending...");
+
+
+// DL - Driving Licence
+// 18 >= You can vote
+// You cannot Vote
+
+// var let const
+const age = "abc"
+
+if(age > 0){
+    if(age >= 18){
+        console.log("You can Vote!");
+    } else {
+        console.log("You cannot Vote!");
+    }
+} else{
+    console.log("Invalid Age!");
+}
+
+
+console.log("END!");
+
+
+// DL - Driving Licence
+
+// 18 >= Can Drive
+// 16 - 18 = Learning DL
+// age > 16 = You cannot Drive
+// EyeWeak = true && age > 40 => You cannot Drive or You must have your Documents
---
+++ switch.js
@@ -0,0 +1,67 @@
+// Switch Case statement!
+
+const num = 10
+
+// if (num === 5) {
+//     console.log("Num is 5");
+// } else if (num === 6) {
+//     console.log("Num is 6");
+// } else if (num === 7) {
+//     console.log("Num is 7");
+// } else {
+//     console.log("Num is not 5,6,7");
+// }
+
+// Logic for chekcing the num value
+// ["read","write"]
+// switch (num) {
+//     case 5:
+//         console.log("Num is 5");
+//         break;
+//     case 10:
+//         console.log("Num is 10");
+//         // break;
+//     case 7:
+//         console.log("Num is 7");
+//         break;
+//     case 8:
+//         break
+//     default:
+//         console.log("Num is not 5,6,7");
+//         // break;
+// }
+// END!
+
+// Task - Days of week
+
+
+// today's day + all the remaning day of this week
+const day = "friday"
+
+switch (day) {
+    case "monday":
+        console.log("Moday");
+        // break;
+    case "tuesday":
+        console.log("tueday");
+        // break;
+    case "wednesday":
+        console.log("wednesday");
+        // break;
+    case "thursday":
+        console.log("thursday");
+        // break;
+    case "friday":
+        console.log("friday");
+        // break;
+    case "saturday":
+        console.log("saturday");
+        // break;
+    case "sunday":
+        console.log("sunday");
+        break;
+
+    default:
+        console.log("Invalid Value");
+        break;
+}
